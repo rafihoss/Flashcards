8efb5473c0722290803412554229ef3fdb9738a3
shRAFI <4186907-shRAFI@users.noreply.replit.com>
2023-09-26 01:17:22 +0000

    flashcard project 2

---
+++ components/Flashcard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useState} from 'react'
+import './flashcard.css'
+
+const Flashcard = ({answer,question, showFlip, setShowFlip}) => {
+  const toggleAnswer = () => {
+    setShowFlip(!showFlip);    
+  }   //toggle the value of showFlip between true and false. Called when flash clicked
+
+  
+  return (
+    //(below) if 'showFlip' is true, 'show-answer' is added to change card appearance
+    <div className={`flashcard ${showFlip ? 'show-answer' : ''}`} onClick={toggleAnswer} >
+      {showFlip ? answer : question}
+      
+    </div>
+
+  )
+}
+
+export default Flashcard
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,86 @@
+import Flashcard from '../components/Flashcard'
+import {useState} from 'react'
 import './App.css'
-import Card from '../components/Card'
-import premBANNER from "../images/prembanner.png" // Update the import path
 
 const App = () => {
+  const cards = [
+    {
+      question: "Before Haaland broke the most goals in a Premier League season, who held that record?", 
+      answer: "Alan Shearer"
+    },
+    {
+      question: "Who won the 2014/2015 Champions League?",
+      answer: "FC Barcelona"
+    },
+    {
+      question: "Which team won the first World Cup?",
+      answer: "Uruguay"
+    },
+    {
+      question: "Who hosted the 2002 World Cup?",
+      answer: "South Korea / Japan"
+    },
+    {
+      question: "Which player assited the goal for the famous 'AGUEROOOO' goal?",
+      answer: "Mario Balotelli"
+    },
+    {
+      question: "Why did Messi have to leave Barcelona?",
+      answer: "Because Barca were broke"
+    },
+    {
+      question: "Who assited Ronaldo for his famous bicycle goal agaisnt Juventes",
+      answer: "Dani Carvaja"
+    },
+    {
+      question: "Who score the winning goal in the 6-1 game between Barcelona and PSG?",
+      answer: "Sergi Roberto"
+    },
+    {
+      question: "Who missed the last penalty in the Europa League Final between Man U and Villareal?",
+      answer: "David De Gea"
+    },
+    {
+      question: "Who is the goat of Football?",
+      answer: "Lionel Messi"
+    },
+  ]
+
+  const [currentCard, setCurrentCard] = useState(0);
+  const [showFlip, setShowFlip] = useState(false);
+     //keeps track of the displayed card. intially 0 to start w/ first card
+
+  const nextCard = () => {
+    
+    //getting random card for the next card
+    const newCard = Math.floor(Math.random() * cards.length);
+    //to make sure new one is not the same as current:
+    if (newCard !== currentCard) {
+    setCurrentCard(newCard);
+    setShowFlip(false); //reset showFlip to false
+    } 
+    else {
+    // if its the same, try it again
+    nextCard();
+    }
+  }
+  
   return (
     <div className="App">
-      <div className="image-container"> 
-        <img src={premBANNER} alt="premBANNER" height="220px" width="55%" className="centered-image" />
-        <h1>Top 10 Team Schedules</h1> 
-      </div>
+      <center>
+      <h1>Football Quiz</h1>
+      <h2>How good is your ball knowledge? Test it out!</h2>
+      <h3>Number of card: 10</h3>   
 
-    <div className='cardboard'>
-      <Card imageUrl={"https://1000logos.net/wp-content/uploads/2017/05/emblem-Manchester-City.jpg"} teamName={"Manchester City"} stadiumName={"Etihad Stadium"} scheduleLink={"https://www.fotmob.com/teams/8456/fixtures/manchester-city"} />
-      
-      <Card imageUrl={"https://i.pinimg.com/originals/8e/f5/9d/8ef59dc3c90a3abd56c87a5901709132.jpg"} teamName={"Manchester United"} stadiumName={"Old Trafford"} scheduleLink={"https://www.fotmob.com/teams/10260/fixtures/manchester-united"} />
-      
-      <Card imageUrl={"https://wallpapercave.com/wp/wp1924030.jpg"} teamName={"Liverpool"} stadiumName={"Anfield"} scheduleLink={"https://www.fotmob.com/teams/8650/fixtures/liverpool"} />
-      
-      <Card imageUrl={"https://c4.wallpaperflare.com/wallpaper/968/357/70/tottenham-hotspur-football-logo-tottenham-wallpaper-preview.jpg"} teamName={"Tottenham Hutspur"} stadiumName={"Tottenham Stadium"} scheduleLink={"https://www.fotmob.com/teams/8586/fixtures/tottenham-hotspur"} />
-      
-      <Card imageUrl={"https://wallpapercave.com/wp/wp7422182.jpg"} teamName={"Arsenal"} stadiumName={"Emirates Stadium"} scheduleLink={"https://www.fotmob.com/teams/9825/fixtures/arsenal"} />
-      
-      <Card imageUrl={"https://wallpapercave.com/wp/XwRmbnm.jpg"} teamName={"Chelsea"} stadiumName={"Stamford Bridge"} scheduleLink={"https://www.fotmob.com/teams/8455/fixtures/chelsea"} />
-      
-      <Card imageUrl={"https://1000logos.net/wp-content/uploads/2018/06/Brighton-Hove-Albion-fc-Logo-768x432.jpg"} teamName={"Brighton & Hove Albion"} stadiumName={"Amex Stadium"} scheduleLink={"https://www.fotmob.com/teams/10204/fixtures/brighton-hove-albion"} />
-      
-      <Card imageUrl={"https://1000logos.net/wp-content/uploads/2018/06/newcastle-united-fc-logo-768x432.jpg"} teamName={"Newcastle United"} stadiumName={"St. James' Park"} scheduleLink={"https://www.fotmob.com/teams/10261/fixtures/newcastle-united"} />
-      
-      <Card imageUrl={"https://www.logodesignlove.com/images/sports/brentford-club-crest-01.jpg"} teamName={"Brentford"} stadiumName={"Gtech Community Stadium"} scheduleLink={"https://www.fotmob.com/teams/9937/fixtures/brentford"} />
-      
-      <Card imageUrl={"https://1000logos.net/wp-content/uploads/2018/06/crystal-palace-new-logo.jpg"} teamName={"Crystal Palace"} stadiumName={"Selhurst Park"} scheduleLink={"https://www.fotmob.com/teams/9826/fixtures/crystal-palace"} />
-      
-    </div>
+      <Flashcard question={cards[currentCard].question} answer={cards[currentCard].answer} showFlip={showFlip} setShowFlip={setShowFlip} />
+
+      <button onClick={nextCard}> →</button>
+        
+      </center>
+     
       </div>
-  );
+  )
 }
+
 
 export default App;
